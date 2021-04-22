f44c50eaa9ddae1bb2379d6bb07a32f866f2687a
MuhammadUsama-crypto <76597663+MuhammadUsama-crypto@users.noreply.github.com>
2021-04-23 00:51:03 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import LandingPage from "./Modules/LandingPage";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+// import "../node_modules/bulma/css/bulma.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage/>
+     
+    </div>
+  );
+}
+
+export default App;
